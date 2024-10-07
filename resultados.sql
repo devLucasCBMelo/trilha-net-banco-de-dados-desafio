b7934f36c514aea1bb78cf4e8788d366bbf58f98
Lucas Costa <englucascbmelo@gmail.com>
2024-10-07 15:15:29 -0300

    requisito 4

--- resultados.sql
+++ resultados.sql
@@ -9,3 +9,8 @@ SELECT `Nome`, `Ano`, `Duracao`
 FROM `Filmes`
 WHERE
     LOWER(`Nome`) LIKE LOWER('%de Volta para o Futuro');
+
+-- Requisto 4 -  Buscar os filmes lançados em 1997
+SELECT `Nome`, `Ano`, `Duracao` FROM `Filmes` WHERE `Ano` = 1997
+
+-- Requisito 5 -  Buscar os filmes lançados APÓS o ano 2000
