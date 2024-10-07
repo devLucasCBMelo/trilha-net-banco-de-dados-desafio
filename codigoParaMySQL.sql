7a81a7fb8f3956be74315eb28a1a42b26b1992c5
Lucas Costa <englucascbmelo@gmail.com>
2024-10-07 15:10:56 -0300

    requisito 3

--- codigoParaMySQL.sql
+++ codigoParaMySQL.sql
@@ -2,6 +2,8 @@ CREATE DATABASE `Filmes`;
 
 USE `Filmes`;
 
+-------------ATORES----------------------
+
 CREATE TABLE `Atores` (
     `Id` INT AUTO_INCREMENT NOT NULL,
     `PrimeiroNome` VARCHAR(20),
@@ -10,6 +12,49 @@ CREATE TABLE `Atores` (
     PRIMARY KEY (`Id`)
 ) ENGINE = InnoDB;
 
+INSERT INTO
+    `Atores` (
+        `Id`,
+        `PrimeiroNome`,
+        `UltimoNome`,
+        `Genero`
+    )
+VALUES (1, 'James', 'Stewart', 'M'),
+    (2, 'Deborah', 'Kerr', 'F'),
+    (3, 'Peter', 'OToole', 'M'),
+    (4, 'Robert', 'DeNiro', 'M'),
+    (6, 'Harrison', 'Ford', 'M'),
+    (7, 'Stephen', 'Baldwin', 'M'),
+    (8, 'Jack', 'Nicholson', 'M'),
+    (9, 'Mark', 'Wahlberg', 'M'),
+    (10, 'Woody', 'Allen', 'M'),
+    (11, 'Claire', 'Danes', 'F'),
+    (12, 'Tim', 'Robbins', 'M'),
+    (13, 'Kevin', 'Spacey', 'M'),
+    (14, 'Kate', 'Winslet', 'F'),
+    (15, 'Robin', 'Williams', 'M'),
+    (16, 'Jon', 'Voight', 'M'),
+    (17, 'Ewan', 'McGregor', 'M'),
+    (18, 'Christian', 'Bale', 'M'),
+    (
+        19,
+        'Maggie',
+        'Gyllenhaal',
+        'F'
+    ),
+    (20, 'Dev', 'Patel', 'M'),
+    (
+        21,
+        'Sigourney',
+        'Weaver',
+        'F'
+    ),
+    (22, 'David', 'Aston', 'M'),
+    (23, 'Ali', 'Astin', 'F');
+
+DROP TABLE `Atores`;
+--------------------FILMES------------------
+
 CREATE TABLE `Filmes` (
     `Id` INT AUTO_INCREMENT NOT NULL,
     `Nome` VARCHAR(50),
@@ -17,76 +62,6 @@ CREATE TABLE `Filmes` (
     `Duracao` INT,
     PRIMARY KEY (`Id`)
 ) ENGINE = InnoDB;
-
-CREATE TABLE `ElencoFilme` (
-    `Id` INT AUTO_INCREMENT NOT NULL,
-    `IdAtor` INT NOT NULL,
-    `IdFilme` INT,
-    `Papel` VARCHAR(30),
-    PRIMARY KEY (`Id`),
-    FOREIGN KEY (`IdAtor`) REFERENCES `Atores` (`Id`),
-    FOREIGN KEY (`IdFilme`) REFERENCES `Filmes` (`Id`)
-) ENGINE = InnoDB;
-
-CREATE TABLE `Generos` (
-    `Id` INT AUTO_INCREMENT NOT NULL,
-    `Genero` VARCHAR(20),
-    PRIMARY KEY (`Id`)
-) ENGINE = InnoDB;
-
-CREATE TABLE `FilmesGenero` (
-    `Id` INT AUTO_INCREMENT NOT NULL,
-    `IdGenero` INT,
-    `IdFilme` INT,
-    PRIMARY KEY (`Id`),
-    FOREIGN KEY (`IdGenero`) REFERENCES `Generos` (`Id`),
-    FOREIGN KEY (`IdFilme`) REFERENCES `Filmes` (`Id`)
-) ENGINE = InnoDB;
-
-INSERT INTO
-    `Generos` (Id, `Genero`)
-VALUES (1, 'Ação'),
-    (2, 'Aventura'),
-    (3, 'Animação'),
-    (4, 'Biografia'),
-    (5, 'Comédia'),
-    (6, 'Crime'),
-    (7, 'Drama'),
-    (8, 'Horror'),
-    (9, 'Musical'),
-    (10, 'Mistério'),
-    (11, 'Romance'),
-    (12, 'Suspense'),
-    (13, 'Guerra');
-
-INSERT INTO
-    `Atores` (
-        `PrimeiroNome`,
-        `UltimoNome`,
-        `Genero`
-    )
-VALUES ('James', 'Stewart', 'M'),
-    ('Deborah', 'Kerr', 'F'),
-    ('Peter', 'OToole', 'M'),
-    ('Robert', 'DeNiro', 'M'),
-    ('Harrison', 'Ford', 'M'),
-    ('Stephen', 'Baldwin', 'M'),
-    ('Jack', 'Nicholson', 'M'),
-    ('Mark', 'Wahlberg', 'M'),
-    ('Woody', 'Allen', 'M'),
-    ('Claire', 'Danes', 'F'),
-    ('Tim', 'Robbins', 'M'),
-    ('Kevin', 'Spacey', 'M'),
-    ('Kate', 'Winslet', 'F'),
-    ('Robin', 'Williams', 'M'),
-    ('Jon', 'Voight', 'M'),
-    ('Ewan', 'McGregor', 'M'),
-    ('Christian', 'Bale', 'M'),
-    ('Maggie', 'Gyllenhaal', 'F'),
-    ('Dev', 'Patel', 'M'),
-    ('Sigourney', 'Weaver', 'F'),
-    ('David', 'Aston', 'M'),
-    ('Ali', 'Astin', 'F');
 
 INSERT INTO
     `Filmes` (`Nome`, `Ano`, `Duracao`)
@@ -146,28 +121,142 @@ VALUES ('Um Corpo que Cai', 1958, 128),
         'Quem Quer Ser um Milionário?',
         2008,
         120
+    ),
+    (
+        'Aliens, O Resgate',
+        1986,
+        137
+    ),
+    (
+        'Uma Vida sem Limites',
+        2004,
+        118
+    ),
+    ('Avatar', 2009, 162),
+    ('Coração Valente', 1995, 178),
+    ('Os Sete Samurais', 1954, 207),
+    (
+        'A Viagem de Chihiro',
+        2001,
+        125
+    ),
+    (
+        'De Volta para o Futuro',
+        1985,
+        116
     );
 
+DROP TABLE `Filmes`;
+
+-------------------ELENCO FILME-----
+
+CREATE TABLE `ElencoFilme` (
+    `Id` INT AUTO_INCREMENT NOT NULL,
+    `IdAtor` INT NOT NULL,
+    `IdFilme` INT,
+    `Papel` VARCHAR(30),
+    PRIMARY KEY (`Id`),
+    FOREIGN KEY (`IdAtor`) REFERENCES `Atores` (`Id`),
+    FOREIGN KEY (`IdFilme`) REFERENCES `Filmes` (`Id`)
+) ENGINE = InnoDB;
+
 INSERT INTO
-    `ElencoFilme` (`IdAtor`, `IdFilme`, `Papel`)
-VALUES (1, 1, 'John Scottie Ferguson'),
-    (2, 2, 'Miss Giddens'),
-    (3, 3, 'T.E. Lawrence'),
-    (4, 4, 'Michael'),
-    (6, 6, 'Rick Deckard'),
-    (7, 8, 'McManus'),
-    (9, 10, 'Eddie Adams'),
-    (10, 11, 'Alvy Singer'),
-    (11, 12, 'San'),
-    (12, 13, 'Andy Dufresne'),
-    (13, 14, 'Lester Burnham'),
-    (14, 15, 'Rose DeWitt Bukater'),
-    (15, 16, 'Sean Maguire'),
-    (16, 17, 'Ed'),
-    (17, 18, 'Renton'),
-    (19, 20, 'Elizabeth Darko'),
-    (20, 21, 'Older Jamal'),
-    (21, 22, 'Ripley'),
-    (22, 23, 'Bobby Darin'),
-    (23, 9, 'J.J. Gittes'),
-    (24, 19, 'Alfred Borden');
+    `ElencoFilme` (
+        `Id`,
+        `IdAtor`,
+        `IdFilme`,
+        `Papel`
+    )
+VALUES (
+        1,
+        1,
+        1,
+        'John Scottie Ferguson'
+    ),
+    (2, 2, 2, 'Miss Giddens'),
+    (3, 3, 3, 'T.E. Lawrence'),
+    (4, 4, 4, 'Michael'),
+    (5, 6, 6, 'Rick Deckard'),
+    (6, 7, 8, 'McManus'),
+    (7, 9, 10, 'Eddie Adams'),
+    (8, 10, 11, 'Alvy Singer'),
+    (9, 11, 12, 'San'),
+    (10, 12, 13, 'Andy Dufresne'),
+    (11, 13, 14, 'Lester Burnham'),
+    (
+        12,
+        14,
+        15,
+        'Rose DeWitt Bukater'
+    ),
+    (13, 15, 16, 'Sean Maguire'),
+    (14, 16, 17, 'Ed'),
+    (15, 17, 18, 'Renton'),
+    (16, 19, 20, 'Elizabeth Darko'),
+    (17, 20, 21, 'Older Jamal'),
+    (18, 21, 22, 'Ripley'),
+    (19, 13, 23, 'Bobby Darin'),
+    (20, 8, 9, 'J.J. Gittes'),
+    (21, 18, 19, 'Alfred Borden');
+
+DROP TABLE `ElencoFilme`;
+
+-------------------GENEROS------------
+
+CREATE TABLE `Generos` (
+    `Id` INT AUTO_INCREMENT NOT NULL,
+    `Genero` VARCHAR(20),
+    PRIMARY KEY (`Id`)
+) ENGINE = InnoDB;
+
+INSERT INTO
+    `Generos` (Id, `Genero`)
+VALUES (1, 'Ação'),
+    (2, 'Aventura'),
+    (3, 'Animação'),
+    (4, 'Biografia'),
+    (5, 'Comédia'),
+    (6, 'Crime'),
+    (7, 'Drama'),
+    (8, 'Horror'),
+    (9, 'Musical'),
+    (10, 'Mistério'),
+    (11, 'Romance'),
+    (12, 'Suspense'),
+    (13, 'Guerra');
+
+DROP TABLE `Generos`
+-----------------FILMES GENERO-------------
+
+CREATE TABLE `FilmesGenero` (
+    `Id` INT AUTO_INCREMENT NOT NULL,
+    `IdGenero` INT,
+    `IdFilme` INT,
+    PRIMARY KEY (`Id`),
+    FOREIGN KEY (`IdGenero`) REFERENCES `Generos` (`Id`),
+    FOREIGN KEY (`IdFilme`) REFERENCES `Filmes` (`Id`)
+) ENGINE = InnoDB;
+
+INSERT INTO
+    FilmesGenero (Id, IdGenero, IdFilme)
+VALUES (1, 1, 22),
+    (2, 2, 17),
+    (3, 2, 3),
+    (4, 3, 12),
+    (5, 5, 11),
+    (6, 6, 8),
+    (7, 6, 13),
+    (8, 7, 26),
+    (9, 7, 28),
+    (10, 7, 18),
+    (11, 7, 21),
+    (12, 8, 2),
+    (13, 9, 23),
+    (14, 10, 7),
+    (15, 10, 27),
+    (16, 10, 1),
+    (17, 11, 14),
+    (18, 12, 6),
+    (19, 13, 4);
+
+DROP TABLE `FilmesGenero`;
--- resultados.sql
+++ resultados.sql
@@ -1,2 +1,11 @@
 -- Requisito 1 - Buscando o nom e o ano dos filmes
-SELECT `Nome`, `Ano` FROM `Filmes`
+SELECT `Nome`, `Ano` FROM `Filmes`;
+
+-- Requisito 2 - Buscando nome e ano dos filmes, ordenados por ordem crescente pelo ano
+SELECT `Nome`, `Ano`, `Duracao` FROM `Filmes` ORDER BY `Ano`;
+
+-- Requisito 3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e duração
+SELECT `Nome`, `Ano`, `Duracao`
+FROM `Filmes`
+WHERE
+    LOWER(`Nome`) LIKE LOWER('%de Volta para o Futuro');
