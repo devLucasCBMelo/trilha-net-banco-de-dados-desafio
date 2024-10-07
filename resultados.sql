73b8517de38ce8a8e75d4c3070d9221d047e6ebd
Lucas Costa <englucascbmelo@gmail.com>
2024-10-07 15:31:49 -0300

    requisito 5

--- resultados.sql
+++ resultados.sql
@@ -14,3 +14,4 @@ WHERE
 SELECT `Nome`, `Ano`, `Duracao` FROM `Filmes` WHERE `Ano` = 1997
 
 -- Requisito 5 -  Buscar os filmes lançados APÓS o ano 2000
+SELECT `Nome`, `Ano`, `Duracao` FROM `Filmes` WHERE `Ano` > 2000;
