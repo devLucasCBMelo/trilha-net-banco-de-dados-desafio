3c90968e7643953d00148b5b84d4e93dc07948b6
Lucas Costa <englucascbmelo@gmail.com>
2024-10-07 15:34:01 -0300

    requisito 6

--- resultados.sql
+++ resultados.sql
@@ -12,6 +12,13 @@ WHERE
 
 -- Requisto 4 -  Buscar os filmes lançados em 1997
 SELECT `Nome`, `Ano`, `Duracao` FROM `Filmes` WHERE `Ano` = 1997
-
 -- Requisito 5 -  Buscar os filmes lançados APÓS o ano 2000
 SELECT `Nome`, `Ano`, `Duracao` FROM `Filmes` WHERE `Ano` > 2000;
+
+-- Requisito 6 - Buscar os filmes com a duração maior que 100 e menor que 150, ordenado pela duração em ordem crescente
+SELECT `Nome`, `Ano`, `Duracao`
+FROM `Filmes`
+WHERE
+    `Duracao` > 100
+    AND `Duracao` < 150
+ORDER BY `Duracao` ASC
